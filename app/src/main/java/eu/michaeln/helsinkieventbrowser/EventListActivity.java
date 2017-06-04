27f10c298c6fdff7824166526801d70eb1a1ceb8
michaelneu <git@michaeln.eu>
2017-06-04 18:34:06 +0200

    add basic layout with listview and toolbar

--- app/src/main/java/eu/michaeln/helsinkieventbrowser/EventListActivity.java
+++ app/src/main/java/eu/michaeln/helsinkieventbrowser/EventListActivity.java
@@ -1,13 +1,33 @@
 package eu.michaeln.helsinkieventbrowser;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.widget.ListView;
 
 public class EventListActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private FloatingActionButton searchFAB;
+    private ListView eventsListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_list);
+
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        searchFAB = (FloatingActionButton)findViewById(R.id.search_fab);
+        eventsListView = (ListView)findViewById(R.id.events_list);
+
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_event_list, menu);
+
+        return true;
     }
 }
