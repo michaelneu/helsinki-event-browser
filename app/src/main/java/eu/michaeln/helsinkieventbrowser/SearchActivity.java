3a3cb85cdc903147d24a87b73534aa937fa57df6
michaelneu <git@michaeln.eu>
2017-06-05 14:24:28 +0200

    adjust search layout

--- app/src/main/java/eu/michaeln/helsinkieventbrowser/SearchActivity.java
+++ app/src/main/java/eu/michaeln/helsinkieventbrowser/SearchActivity.java
@@ -1,15 +1,29 @@
 package eu.michaeln.helsinkieventbrowser;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class SearchActivity extends AppCompatActivity {
     private Toolbar toolbar;
-    private AutoCompleteTextView keyword;
+    private AutoCompleteTextView event, keyword, place;
+    private TextView date;
+
+    private Calendar calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +31,43 @@ public class SearchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_search);
 
         toolbar = (Toolbar)findViewById(R.id.toolbar);
+        event = (AutoCompleteTextView)findViewById(R.id.event);
         keyword = (AutoCompleteTextView)findViewById(R.id.keyword);
+        place = (AutoCompleteTextView)findViewById(R.id.place);
+        date = (TextView)findViewById(R.id.date);
 
         setSupportActionBar(toolbar);
         final ActionBar actionBar = getSupportActionBar();
 
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setTitle(R.string.search);
+
+        calendar = Calendar.getInstance();
+
+        date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final int year = calendar.get(Calendar.YEAR),
+                        month = calendar.get(Calendar.MONTH),
+                        day = calendar.get(Calendar.DAY_OF_MONTH);
+
+                final DatePickerDialog dialog = new DatePickerDialog(SearchActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                        calendar.set(Calendar.YEAR, year);
+                        calendar.set(Calendar.MONTH, month);
+                        calendar.set(Calendar.DAY_OF_MONTH, day);
+
+                        final SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yyyy", Locale.ENGLISH);
+                        final String formattedDate = formatter.format(calendar.getTime());
+
+                        date.setText(formattedDate);
+                    }
+                }, year, month, day);
+
+                dialog.show();
+            }
+        });
     }
 
     @Override
@@ -32,4 +76,22 @@ public class SearchActivity extends AppCompatActivity {
 
         return true;
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.search:
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
