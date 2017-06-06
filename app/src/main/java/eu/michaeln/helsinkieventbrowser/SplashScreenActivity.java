8daf007e0dc6fb78fac579945fcca61c195010ac
michaelneu <git@michaeln.eu>
2017-06-06 15:34:16 +0200

    allow short-curcuiting the splash screen via tap

--- app/src/main/java/eu/michaeln/helsinkieventbrowser/SplashScreenActivity.java
+++ app/src/main/java/eu/michaeln/helsinkieventbrowser/SplashScreenActivity.java
@@ -2,25 +2,43 @@ package eu.michaeln.helsinkieventbrowser;
 
 import android.content.Intent;
 import android.os.Handler;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SplashScreenActivity extends AppCompatActivity {
     private static final int SPLASH_DURATION = 3000;
+
+    private boolean switchedActivity = false;
+
+    private final Runnable continueRunnable = new Runnable() {
+        @Override
+        public void run() {
+            if (!switchedActivity) {
+                switchedActivity = true;
+                final Intent activitySwitchIntent = new Intent(SplashScreenActivity.this, EventListActivity.class);
+
+                startActivity(activitySwitchIntent);
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        final Handler delayActivityStartHandler = new Handler();
-        delayActivityStartHandler.postDelayed(new Runnable() {
+        final ConstraintLayout splashWrapper = (ConstraintLayout)findViewById(R.id.splash_wrapper);
+        splashWrapper.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                final Intent activitySwitchIntent = new Intent(SplashScreenActivity.this, EventListActivity.class);
-
-                startActivity(activitySwitchIntent);
+            public void onClick(View view) {
+                continueRunnable.run();
             }
-        }, SPLASH_DURATION);
+        });
+
+        final Handler delayActivityStartHandler = new Handler();
+
+        delayActivityStartHandler.postDelayed(continueRunnable, SPLASH_DURATION);
     }
 }
